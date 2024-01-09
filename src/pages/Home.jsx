3c0eae3b6194e4c637a833cd2c8a6dbe8d63f93b
Gabirel Angello <142102428+GabrielGotinha@users.noreply.github.com>
2024-01-09 09:24:03 -0300

    fim

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import "./MoviesGrid.css";
+
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  console.log(topMovies);
+
+
+    return (
+        <div className="container">
+            <h2 className="title">Melhores Filme:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie.id}/>)}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
